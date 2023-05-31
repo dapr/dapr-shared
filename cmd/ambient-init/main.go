b391e3944d95d40a89ed844cc98d2cc43f4cba81
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-05-31 01:23:37 -0300

    refactor: accept pr suggestions

--- cmd/ambient-init/main.go
+++ cmd/ambient-init/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/salaboy/dapr-ambient/internal/ambient"
+	"github.com/dapr/dapr/pkg/injector/sidecar"
+	daprutils "github.com/dapr/dapr/utils"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	configMapName                  string = "dapr-ambient-configmap"
+	daprTrustAnchorsConfigMapKey   string = "dapr-trust-anchors"
+	daprTrustCertChainConfigMapKey string = "dapr-cert-chain"
+	daprTrustCertKeyConfigMapKey   string = "dapr-cert-key"
+	namespaceDefault               string = "default"
+	DaprSystemNamespace            string = "dapr-system"
+	DaprControlPlaneNamespace      string = "DAPR_CONTROL_PLANE_NAMESPACE"
 )
 
 func main() {
@@ -12,13 +29,63 @@ func main() {
 
 	ctx := context.Background()
 
-	cs := ambient.NewClientset()
+	kubeClient := daprutils.GetKubeClient()
 
-	c := ambient.NewDaprSidecarClient(cs)
+	c := NewDaprSidecarClient(kubeClient)
 
-	trustBundle := c.Get(ctx, ambient.LookupEnvOrString(ambient.DaprControlPlaneNamespace, ambient.DaprControlPlaneDefaultNamespace))
+	rootCert, certChain, certKey := c.Get(ctx, LookupEnvOrString(DaprControlPlaneNamespace, DaprSystemNamespace))
 
-	k := ambient.NewK8SClient(cs)
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      configMapName,
+			Namespace: namespaceDefault,
+		},
+		Data: map[string]string{
+			daprTrustAnchorsConfigMapKey:   rootCert,
+			daprTrustCertChainConfigMapKey: certChain,
+			daprTrustCertKeyConfigMapKey:   certKey,
+		},
+	}
 
-	k.CreateConfigMap(ctx, ambient.DefaultNamespace, trustBundle.ToMap())
+	_, err := kubeClient.CoreV1().ConfigMaps(namespaceDefault).Get(ctx, configMapName, metav1.GetOptions{})
+	if err == nil {
+		log.Println(fmt.Printf("configmap %s already exists", configMapName))
+	}
+
+	_, err = kubeClient.CoreV1().ConfigMaps(namespaceDefault).Create(ctx, configMap, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// LookupEnvOrString tries to look for an environment variable, if found, return it, otherwise find,
+// return the default parameter.
+func LookupEnvOrString(key string, defaultVal string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultVal
+}
+
+// DaprSidecarClient represents a client to get Dapr trust bundle.
+type DaprSidecarClient interface {
+	Get(ctx context.Context, ns string) (string, string, string)
+}
+
+type daprSidecarClient struct {
+	Clientset *kubernetes.Clientset
+}
+
+// NewDaprSidecarClient creates a DaprSidecarClient instance.
+func NewDaprSidecarClient(cs *kubernetes.Clientset) DaprSidecarClient {
+	return &daprSidecarClient{
+		Clientset: cs,
+	}
+}
+
+// Get gets DaprTrustBundle struct.
+func (s *daprSidecarClient) Get(ctx context.Context, ns string) (string, string, string) {
+	daprCPNamespace := LookupEnvOrString(DaprControlPlaneNamespace, DaprSystemNamespace)
+	return sidecar.GetTrustAnchorsAndCertChain(ctx, s.Clientset, daprCPNamespace)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/dapr/dapr v1.10.4
+	k8s.io/api v0.27.2
+	k8s.io/apimachinery v0.27.2
 	k8s.io/client-go v0.27.2
 )
 
@@ -25,12 +27,10 @@ require (
 	github.com/jhump/protoreflect v1.14.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tidwall/transform v0.0.0-20201103190739-32f242e2dbde // indirect
@@ -45,12 +45,9 @@ require (
 	google.golang.org/genproto v0.0.0-20230124163310-31e0e69b6fc2 // indirect
 	google.golang.org/grpc v1.52.3 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.2 // indirect
-	k8s.io/apimachinery v0.27.2 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230525220651-2546d827e515 // indirect
 	k8s.io/utils v0.0.0-20230505201702-9f6742963106 // indirect
